1b50600d4c6390ec2dbf177f674dec8d8f7293e5
Will <wor101@gmail.com>
2020-12-29 23:02:46 +0000

    more excercises from Ruby objects book

--- good_dog.rb
+++ good_dog.rb
@@ -1,25 +1,29 @@
-module Speak
-  def speak(sound)
-    puts sound
+class GoodDog
+  attr_accessor :name, :height, :weight
+  
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
+  end
+  
+  def speak
+    "#{self.name} says arf!"
+  end
+  
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+  
+  def info
+     "#{self.name} weighs #{self.weight} and is #{self.height} tall."
   end
 end
 
+sparky = GoodDog.new("Sparky", '12 inches', '10 lbs')        
+puts sparky.info
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info
 
-class GoodDog
-  include Speak
-end
-
-class HumanBeing
-  include Speak
-end
-
-sparky = GoodDog.new
-sparky.speak("Arf!")
-bob = HumanBeing.new
-bob.speak("Hello!")
-
-puts "---GoodDog ancestors---"
-puts GoodDog.ancestors
-puts ' '
-puts "---HumanBeing ancestors"
-puts HumanBeing.ancestors
