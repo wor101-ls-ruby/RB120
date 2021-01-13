2b1eb3021f6f27eebbe3328f2118e8841084bf66
Will <wor101@gmail.com>
2021-01-13 19:34:13 +0000

    functioning tic tac toe

--- lesson_5/oottt.rb
+++ lesson_5/oottt.rb
@@ -1,11 +1,13 @@
 require 'pry'
 
 class Board
-  INITIAL_MARKER = " "
+  WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]]              # diagnols
 
   def initialize
     @squares = {}
-    (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER) }
+    reset
   end
 
   def get_square_at(key)
@@ -15,17 +17,59 @@ class Board
   def set_square_at(key, marker)
     @squares[key].marker = marker
   end
+  
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
+  end
+  
+  def full?
+    unmarked_keys.empty?
+  end
+  
+  def someone_won?
+    !!detect_winner
+  end
+  
+  def count_human_marker(squares)
+    squares.collect(&:marker).count(TTTGame::HUMAN_MARKER)
+  end
+  
+  def count_computer_marker(squares)
+    squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
+  end
+  # returns the winning marker or nil
+  def detect_winner
+    WINNING_LINES.each do |line|
+
+      if count_human_marker(@squares.values_at(*line)) == 3
+        return TTTGame::HUMAN_MARKER
+      elsif count_computer_marker(@squares.values_at(*line)) == 3
+        return TTTGame::COMPUTER_MARKER
+      end
+    end
+    nil
+  end
+  
+  def reset
+    (1..9).each { |key| @squares[key] = Square.new }
+  end
 end
 
 class Square
+  INITIAL_MARKER = " "
+  
   attr_accessor :marker
 
-  def initialize(marker)
+  def initialize(marker = INITIAL_MARKER)
     @marker = marker
   end
   
   def to_s
     @marker
+  end
+  
+  def unmarked?
+    marker == INITIAL_MARKER
   end
 end
 
@@ -38,12 +82,15 @@ class Player
 end
 
 class TTTGame
+  HUMAN_MARKER = "X"
+  COMPUTER_MARKER ="O"
+
   attr_reader :board, :human, :computer
   
   def initialize
     @board = Board.new
-    @human = Player.new('X')
-    @computer = Player.new('O')
+    @human = Player.new(HUMAN_MARKER)
+    @computer = Player.new(COMPUTER_MARKER)
   end
   
   def display_welcome_message
@@ -55,7 +102,9 @@ class TTTGame
     puts "Thanks for playing Tic Tac Toe! Goodbye!"
   end
   
-  def display_board
+  def display_board(clear = true)
+    system 'clear' if clear
+    puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
     puts "     |     |"
     puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}  "
@@ -72,28 +121,67 @@ class TTTGame
   end
   
   def human_moves
-    puts "Choose a square between 1-9: "
+    puts "Choose a square (#{board.unmarked_keys.join(', ')}): "
     square = nil
     loop do
       square = gets.chomp.to_i
-      break if (1..9).include?(square)
+      break if board.unmarked_keys.include?(square)
       puts "Sorry, that's not a valid choice."
     end
     
     board.set_square_at(square, human.marker)
   end
   
+  def computer_moves
+    board.set_square_at(board.unmarked_keys.sample, computer.marker)
+  end
+  
+  def display_result
+    display_board
+    case board.detect_winner
+    when HUMAN_MARKER
+      puts "You won!"
+    when COMPUTER_MARKER
+      puts "Computer won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+  
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp.downcase
+      break if %w(y n).include? answer
+      puts "Sorry, must be y or n"
+    end
+    
+    answer == 'y'
+  end
+  
   def play
     display_welcome_message
+    
     loop do
-      display_board
-      human_moves
-      break if someone_won? || board_full?
-      
-      computer_moves
-      break if someone_won? || board_full?
+    display_board(false)
+    
+      loop do
+        human_moves
+        break if board.someone_won? || board.full?
+  
+        computer_moves
+        break if board.someone_won? || board.full?
+        
+        display_board
+      end
+      display_result
+      break unless play_again?
+      board.reset
+      system 'clear'
+      puts "Let's play again!"
+      puts ""
     end
-    #display_result
     display_goodbye_message
   end
 end
